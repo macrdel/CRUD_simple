ef67f87b595daa5a1911513162c312bef784d657
macrdel <kutdusoff14@gmail.com>
2024-06-24 17:30:38 +0300

    create crud customers

---
+++ app/crud/customers.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from .models import Customer
+from .schemas import CustomerCreate, CustomerUpdate
+
+def create_customer(db: Session, customer: CustomerCreate):
+    db_customer = Customer(**customer.dict())
+    db.add(db_customer)
+    db.commit()
+    db.refresh(db_customer)
+    return db_customer
+
+def get_customer(db: Session, customer_id: int):
+    return db.query(Customer).filter(Customer.id == customer_id).first()
+
+def get_customers(db: Session):
+    return db.query(Customer).all()
+
+def update_customer(db: Session, customer_id: int, customer: CustomerUpdate):
+    db_customer = db.query(Customer).filter(Customer.id == customer_id).first()
+    if not db_customer:
+        return None
+    for var, value in vars(customer).items():
+        setattr(db_customer, var, value) if value else None
+    db.commit()
+    db.refresh(db_customer)
+    return db_customer
+
+def delete_customer(db: Session, customer_id: int):
+    db_customer = db.query(Customer).filter(Customer.id == customer_id).first()
+    if db_customer:
+        db.delete(db_customer)
+        db.commit()
+    return db_customer
