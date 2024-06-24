4875ecc0682626ff9a7b6f382e8dc4a28bbe0690
macrdel <kutdusoff14@gmail.com>
2024-06-24 18:06:17 +0300

    create crud products

---
+++ app/crud/products.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from ..models import Product
+from ..schemas import ProductCreate, ProductUpdate
+
+def create_product(db: Session, product: ProductCreate):
+    db_product = Product(**product.dict())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def get_product(db: Session, product_id: int):
+    return db.query(Product).filter(Product.id == product_id).first()
+
+def get_products(db: Session):
+    return db.query(Product).all()
+
+def update_product(db: Session, product_id: int, product: ProductUpdate):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    if not db_product:
+        return None
+    for var, value in vars(product).items():
+        setattr(db_product, var, value) if value else None
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def delete_product(db: Session, product_id: int):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    if db_product:
+        db.delete(db_product)
+        db.commit()
+    return db_product
