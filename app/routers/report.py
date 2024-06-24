f3bc52c82b736d0056c966b4f08591068dff64f4
macrdel <kutdusoff14@gmail.com>
2024-06-24 22:57:50 +0300

    update report router

--- app/routers/report.py
+++ app/routers/report.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
 from datetime import date
-from ..src.report import get_report, Report
+from ..src.report import get_report as gr, Report
 from ..dependencies import get_db
 
 router = APIRouter()
 
 @router.get("/", response_model=List[Report])
 def get_report(purchase_date: date, db: Session = Depends(get_db)):
-    return get_report(db=db, purchase_date=purchase_date)
+    return gr(db=db, purchase_date=purchase_date)
